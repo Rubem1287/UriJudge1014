ea27bea3b231fcefc841ced378ede5695a08fdfc
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-14 18:51:36 -0300

    Add files via upload

---
+++ Uri 1014/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+namespace Uri_1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            double y, valor, valor1;
+
+            Console.WriteLine();
+            Console.Write("Digite a distância percorrida : ");
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Digite o valor total do combustível : ");
+            y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            valor = x / y;
+
+            //valor1 = valor / x;
+
+            Console.WriteLine();
+            Console.Write(valor.ToString("F3", CultureInfo.InvariantCulture)+" km/l");
+           
+        }
+
+    }
+    
+    
+}
